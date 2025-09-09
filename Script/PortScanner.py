61d55e9adbbf3e80d3d9773d055bf85663057887
ZX <joanna050393@gmail.com>
2025-09-09 22:04:45 +0800

    建立還原點(訊息可以變更)

---
+++ Script/PortScanner.py
@@ -0,0 +1,21 @@
+import socket
+
+target = "127.0.0.1"
+
+common_ports = [21, 22, 23, 25, 53, 80, 110, 139, 143, 443, 3389]
+
+print(f"Scanning {target}...\n")
+
+for port in common_ports:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(1)
+    try:
+        result = sock.connect_ex(target ,port)
+        if result ==0:
+            print(f"Port {port} is OPEN")
+        else:
+            print(f"Port {port} is CLOSE")
+    except Exception as e:
+        print(f"Error scanning port {port}: {e}")
+    finally:
+        sock.close()
